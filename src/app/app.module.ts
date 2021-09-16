66658f02ca96c1da148fc01ae19c122cbc9ae9c3
Aurelien.Weggen <aurelien.weggen@gmail.com>
2021-09-16 11:38:15 +0200

    Premiere partie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitreComponent } from './titre/titre.component';
+import { PersonnesActivesComponent } from './personnes-actives/personnes-actives.component';
+import { NouveauMessageComponent } from './nouveau-message/nouveau-message.component';
+import { AnciensMessagesComponent } from './anciens-messages/anciens-messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitreComponent,
+    PersonnesActivesComponent,
+    NouveauMessageComponent,
+    AnciensMessagesComponent
   ],
   imports: [
     BrowserModule
